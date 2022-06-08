6c8132e98416dfce95533d0cb82baafb9afbafcc
FOx <vip.youtobe2016@mail.ru>
2022-06-08 17:25:51 +0300

    Добавьте файлы проекта.

---
+++ 13.4.cpp
@@ -0,0 +1,15 @@
+﻿
+
+#include <iostream>
+#include "Helpers.h"
+
+
+int main()
+{
+	int summ = (5 + 6) * (5 + 6);
+	int result = summ;
+
+	std::cout << "Result = " << result << std::endl;
+
+}
+
---
+++ Helpers.h
@@ -0,0 +1,10 @@
+
+int sum(int a, int b)
+{
+    int summ = sum(5, 6);
+    
+   
+
+    return (a + b) * (a + b);
+}
+
